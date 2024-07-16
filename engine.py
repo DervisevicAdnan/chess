ca8f72ba722ff678c901faef6b0de9e33a765361
NedimBecic <nbecic1@etf.unsa.ba>
2024-07-16 17:15:46 +0200

    Dodano full enkodiranje i dekodiranje FEN notacije

--- engine.py
+++ engine.py
@@ -38,7 +38,7 @@ class Board:
     '''
 
     def __init__(self):
-        starting_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+        starting_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
         self.board = []
         self.set_position(starting_FEN)
         
@@ -48,6 +48,10 @@ class Board:
         self.is_check = {color.BLACK: False, color.WHITE: False}
         self.long_castle_allowed = {color.BLACK: True, color.WHITE: True}
         self.short_castle_allowed =  {color.BLACK: True, color.WHITE: True}
+
+        self.active_color_move = color.WHITE
+        self.half_moves = 0
+        self.moves_num = 1
 
         self.update_valid_moves()
         # ideja je da previous_moves bude lista tuplova (Figure, prev_x, prev_y, new_x, new_y)
@@ -72,15 +76,54 @@ class Board:
             print(row_line)
 
     def decode_FEN(self, fen_notation):
+
+        split = fen_notation.split()
+        board_position = split[0]
+        active_color_move = split[1]
+        if active_color_move == 'w':
+            self.active_color_move = color.WHITE
+        else:
+            self.active_color_move = color.BLACK
+        castling = split[2]
+
+        # Mogucnosti [('-'), ('KQkq'), ('Kkq'), ('Qkq'), ('KQk'), ('KQq'), ('KQ'), ('kq'), ('Kk'), ('Kq'), ('Qk'), ('Qq'), ('K'), ('Q'), ('k'), ('q')]
+
+        self.long_castle_allowed = {color.BLACK: False, color.WHITE: False}
+        self.short_castle_allowed =  {color.BLACK: False, color.WHITE: False}
+        
+        if 'K' in castling:
+            self.short_castle_allowed[color.WHITE] = True
+        if 'Q' in castling:
+            self.long_castle_allowed[color.WHITE] = True
+        if 'k' in castling:
+            self.short_castle_allowed[color.BLACK] = True
+        if 'q' in castling: 
+            self.long_castle_allowed[color.BLACK] = True
+
+        en_passant = split[3]
+
+        if en_passant[0] != '-':
+            file = notation_decode.get(en_passant[0], "Invalid move")
+            rank = 8 - int(en_passant[1])
+            self.board[rank][file] = EnPassantEmptyField()
+
+        # Ovo je za brojanje koliko je proslo od zadnjeg saha ili uzimanja figure -> kasnije dodat kad se omoguci igranje
+        self.half_moves = int(split[4])
+        # Ovo je za brojanje ukupnih poteza, update the poslije poteza crnog
+        self.moves_num = int(split[5])
+
         board_row = 0
         board_col = 0
-        for character in fen_notation:
+        for character in board_position:
             if character == '/':
                 board_row += 1
                 board_col = 0
             elif character.isdigit():
                 for _ in range(int(character)):
-                    self.board[board_row][board_col] = EmptyField()
+                    if isinstance(self.board[board_row][board_col], EnPassantEmptyField):
+                        continue
+                    else:
+                        self.board[board_row][board_col] = EmptyField()
                     board_col += 1
             else:
                 if character == 'p':
@@ -111,16 +154,24 @@ class Board:
 
     def encode_FEN(self):
         fen_notation=""
-        for row in self.board:
+        en_passant_field="-"
+        for row_index, row in enumerate(self.board):
             empty_field_counter = 0
-            for square in row:
-                if isinstance(square, EmptyField):
+            for col_index, square in enumerate(row):
+                if isinstance(square, EnPassantEmptyField):
+                    file = ""
+                    for key, val in notation_decode.items():
+                        if val == col_index:
+                            file = key
+                    en_passant_field = file + str(8 - row_index)
+                    empty_field_counter += 1
+                elif isinstance(square, EmptyField):
                     empty_field_counter += 1
                 else:
                     if empty_field_counter > 0:
                         fen_notation += str(empty_field_counter)
                         empty_field_counter = 0
-                    if square.color == 1:
+                    if square.color == color.WHITE:
                         if isinstance(square, Pawn):
                             fen_notation += 'P'
                         elif isinstance(square, Rook):
@@ -149,7 +200,27 @@ class Board:
             if empty_field_counter > 0:
                     fen_notation += str(empty_field_counter)
                     empty_field_counter = 0
-            fen_notation += '/'
+            if row_index != 7:
+                fen_notation += '/'
+        
+        if self.active_color_move == color.WHITE:
+            fen_notation += ' ' + 'w'
+        else:
+            fen_notation += ' ' + 'b'
+        
+        castle_rights = ""
+        if self.short_castle_allowed[color.WHITE] == True:
+            castle_rights += 'K'
+        if self.long_castle_allowed[color.WHITE] == True:
+            castle_rights += 'Q'
+        if self.short_castle_allowed[color.BLACK] == True:
+            castle_rights += 'k'
+        if self.long_castle_allowed[color.BLACK] == True:
+            castle_rights += 'q'  
+        if len(castle_rights) == 0:
+            castle_rights += '-'
+
+        fen_notation += ' ' + castle_rights + ' ' + en_passant_field + ' ' + str(self.half_moves) + ' ' + str(self.moves_num)
         return fen_notation
     
     def get_all_valid_moves(self):
@@ -299,6 +370,7 @@ class Board:
                 break
             else:
                 break
+        
         return valid
     
     def get_knight_valid_moves(self, x, y):
@@ -312,7 +384,8 @@ class Board:
                     valid.append((x, y, tmp_x, tmp_y))
                 elif isinstance(self.board[tmp_x][tmp_y], Figure) and self.board[tmp_x][tmp_y].color != self.board[x][y].color:
                     valid.append((x, y, tmp_x, tmp_y))
-        return valid
+        if valid:
+            return valid
     
     def get_king_valid_moves(self, x, y):
         valid = []
@@ -332,8 +405,9 @@ class Board:
     
     def is_field_atacked(self, x, y, king_color):
         for moves in self.valid_moves_by_color[color((king_color.value+1)%2)]:
-            if moves[-2] == x and moves[-1] == y:
-                return True
+            if moves:
+                if moves[-2] == x and moves[-1] == y:
+                    return True
         return False
 
 class Field:
@@ -405,10 +479,14 @@ class Pawn(Figure):
         return ("w" if self.color == color.WHITE else "b") + "P"
 
 b = Board()
+b.set_position('1r1qk2r/pbpp1ppp/1pn2n2/2b1p3/2B1P3/1PN2N2/PBPP1PPP/1R1Q1RK1 b k - 7 8')
 b.print()
 
 print('\n')
 
 print(b.encode_FEN())
-
+list = b.valid_moves_by_color[color.WHITE]
+list = [x for x in list if x != []]
+list = [x for x in list if x != [[], []]]
+print(len(list))
 print('\n')
